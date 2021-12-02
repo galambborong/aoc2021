9525f2e50b4682af413c4c0089b94bd523c80ac2
Peter Keenan <keenan@posteo.net>
2021-12-02 09:01:08 +0000

    Solution for second star

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,24 +1,22 @@
 module DayTwo
 
-type Instruction = {
-    direction: string
-    units: int
-}
-    
+type Instruction = { direction: string; units: int }
+
 let mapInstructionToPosition currentPosition instruction =
-    let (horizontal,depth) = currentPosition
-    match instruction.direction, instruction.units with
-    | "forward", _ -> (horizontal + instruction.units, depth)
-    | "up", _ -> (horizontal, depth - instruction.units)
-    | "down", _ -> (horizontal, depth + instruction.units)
-    | _ -> failwith "todo"
-    
-let multiplyFinalPosition (x, y) = x * y
-    
-let dive (instructions: Instruction[]) =
+    let horizontal, depth, aim = currentPosition
+
+    match instruction.direction with
+    | "forward" -> (horizontal + instruction.units, depth + (aim * instruction.units), aim)
+    | "up" -> (horizontal, depth, aim - instruction.units)
+    | "down" -> (horizontal, depth, aim + instruction.units)
+    | _ -> failwith "This case should never occur"
+
+let multiplyFinalPosition (x, y, _) = x * y
+
+let dive (instructions: Instruction []) =
     instructions
-    |> Array.fold mapInstructionToPosition (0,0)
+    |> Array.fold mapInstructionToPosition (0, 0, 0)
     |> multiplyFinalPosition
-    
+
 // dive exampleInput
 // dive actualInput

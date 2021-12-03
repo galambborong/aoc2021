9178acf0d9abcf2e576d3b8e5e5243d53d744433
Peter Keenan <peter.keenan@sage.com>
2021-12-03 11:19:41 +0000

    Reformat Day3 file

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -4,8 +4,8 @@ open System
 open System.IO
 
 
-let exampleFile = @"c:/dev/aoc2021/day3/exampleInput.txt"
-let actualFile = @"c:/dev/aoc2021/day3/actualInput.txt"
+let exampleFile = @"./Day3/exampleInput.txt"
+let actualFile = @"./Day3/actualInput.txt"
 
 let fileInput file = seq { yield! File.ReadLines file }
 
@@ -15,56 +15,53 @@ let actualInput = fileInput actualFile
 type Rate =
     | Gamma
     | Epsilon
-    
+
 let countNumberOf1sAnd0s char =
-        match char with
-        | '1' -> 1
-        | '0' -> 0
-        | _ -> failwith "This should never occur"
-    
-let countBits binaryInputs index = 
-        binaryInputs
-        |> Seq.map (fun (binaryInput: string) -> binaryInput.[index])
-        |> Seq.countBy countNumberOf1sAnd0s
-        |> Seq.toList
+    match char with
+    | '1' -> 1
+    | '0' -> 0
+    | _ -> failwith "This should never occur"
+
+let countBits binaryInputs index =
+    binaryInputs
+    |> Seq.map (fun (binaryInput: string) -> binaryInput.[index])
+    |> Seq.countBy countNumberOf1sAnd0s
+    |> Seq.toList
 
 let countPredominantBitByIndex binaryInputs (index: int) rateType =
     let count = countBits binaryInputs index
-    
+
     match rateType with
-    | Gamma -> 
+    | Gamma ->
         match count with
-        | [(n, x); (nn, y)] when x > y ->
-            n |> string
-        | [(n, x); (nn, y)] when x < y ->
-            nn |> string
+        | [ (n, x); (_, y) ] when x > y -> n |> string
+        | [ (_, x); (nn, y) ] when x < y -> nn |> string
         | _ -> failwith $"x = {fst count.[0]}; y = {fst count.[1]}"
     | Epsilon ->
         match count with
-        | [(n, x); (nn, y)] when x > y ->
-            nn |> string
-        | [(n, x); (nn, y)] when x < y ->
-            n |> string
+        | [ (_, x); (nn, y) ] when x > y -> nn |> string
+        | [ (n, x); (_, y) ] when x < y -> n |> string
         | _ -> failwith $"x = {fst count.[0]}; y = {fst count.[1]}"
-    
-    
+
+
 let produceNumberFromBinary (binaryInput: seq<string>) rateType =
     let len =
-        binaryInput
-        |> Seq.head
-        |> String.length
-        |> (+) -1
-        
+        binaryInput |> Seq.head |> String.length |> (+) -1
+
     let binaryString =
-        seq { for i in 0..len do
-                  countPredominantBitByIndex binaryInput i rateType }
+        seq {
+            for i in 0 .. len do
+                countPredominantBitByIndex binaryInput i rateType
+        }
         |> String.concat ""
-    
+
     Convert.ToInt32(binaryString, 2)
-    
-    
-let calculatePowerConsumption example = produceNumberFromBinary example Gamma  * produceNumberFromBinary example Epsilon
-    
-    
+
+
+let calculatePowerConsumption example =
+    produceNumberFromBinary example Gamma
+    * produceNumberFromBinary example Epsilon
+
+
 // calculatePowerConsumption exampleInput
 // calculatePowerConsumption actualInput

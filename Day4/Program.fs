c180ce45e96bcdac0901400f7f2ee848577f0c91
Peter Keenan <keenan@posteo.net>
2021-12-04 19:42:39 +0000

    First star working horizontal only

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -1,6 +1,5 @@
 module DayFour
 
-open System
 open System.IO
 
 let readData filePath = seq { yield! File.ReadLines filePath }
@@ -47,81 +46,87 @@ let matchNumber x (n: string, bool: bool) =
     | x when x <> n -> (n, bool)
     | _ -> failwith "Unhandled case"
 
-let exampleThing = 
-  [[("22", false); ("13", false); ("17", false); ("11", false); ("0", false)];
-   [("8", false); ("2", false); ("23", false); ("4", false); ("24", false)];
-   [("21", true); ("9", true); ("14", true); ("16", true); ("7", true)];
-   [("6", false); ("10", false); ("3", false); ("18", false); ("5", false)];
-   [("1", false); ("12", false); ("20", false); ("15", false); ("19", false)]]
+let exampleThing =
+    [ [ ("22", false)
+        ("13", true)
+        ("17", false)
+        ("11", false)
+        ("0", false) ]
+      [ ("8", false)
+        ("2", false)
+        ("23", false)
+        ("4", false)
+        ("24", false) ]
+      [ ("21", true)
+        ("9", true)
+        ("14", true)
+        ("16", true)
+        ("7", true) ]
+      [ ("6", false)
+        ("10", false)
+        ("3", false)
+        ("18", false)
+        ("5", false) ]
+      [ ("1", false)
+        ("12", true)
+        ("20", false)
+        ("15", false)
+        ("19", false) ] ]
 
 let checkRow (row: (string * bool) list) : string list option =
     match row with
     | [ (_, true); (_, true); (_, true); (_, true); (_, true) ] -> Some(row |> List.map fst)
     | _ -> None
 
-//let generateColumn board : string list option =
 let totalBoardSum (board: (string * bool) list list) =
     board
     |> List.map (List.map (fst >> int))
     |> List.map List.sum
     |> List.sum
 
-let checkBoard (board: (string * bool) list list) =
-    printfn "Checking board"
-//    let boardRow = board |> List.map checkRow
-    let boardRow = seq { for i in 0 .. (board.Length - 1) do board.[i] |> checkRow } |> Seq.toList |> List.filter (fun x -> x <> None)
-    
+let unmarkedSum (board: (string * bool) list list) =
+    board
+    |> List.map (List.filter (fun (_, y) -> y = true))
+    |> List.map (List.map (fst >> int))
+    |> List.map List.sum
+    |> List.sum
 
-    printfn $"Matching boardRow {boardRow}"
+
+let checkBoard (board: (string * bool) list list) =
+    let boardRow =
+        seq {
+            for i in 0 .. (board.Length - 1) do
+                board.[i] |> checkRow
+        }
+        |> Seq.toList
+        |> List.filter (fun x -> x <> None)
+
+
     match boardRow with
-    | [ Some list ] ->
-        let boardSum = totalBoardSum board
-        printfn $"Board sum: {boardSum}"
-        let winningRow = list |> List.map int |> List.sum
-        printfn $"Winning row: {winningRow}"
-        totalBoardSum board - (list |> List.map int |> List.sum)
-        
-    //        (board
-//         |> List.map (List.map (fst >> int))
-//         |> List.map List.sum) -
-//        (list
-//         |> List.map int
-//         |> List.sum
+    | [ Some _ ] ->
+        totalBoardSum board - unmarkedSum board
+
     | [ None ] -> 0
     | _ -> 0
-
-// checkBoard exampleThing
 
 let checkNumber bingoBoard x =
     bingoBoard
     |> List.map (applyFunctionToNestedList (matchNumber x))
 
-
-let calculateVictory numberList callingNumber =
-    numberList
-    |> List.map int
-    |> List.sum
-    |> (*) (int callingNumber)
-
 let playBingo (numbers: string list) boards =
     let finalCall = numbers.Length - 1
-
-    let helperFunc board = board |> List.map (List.map checkRow)
 
     let rec callNumber n boards =
         match n <= finalCall with
         | true ->
             let newBoard = checkNumber boards numbers.[n]
 
-            let checkedRows =
-                (helperFunc newBoard)
-                |> List.map (List.filter (fun x -> x <> None))
-                |> List.concat
-                |> List.head
+            let checkedBoards =
+                newBoard |> List.map checkBoard |> List.sum
 
-            match checkedRows with
-            | Some list -> calculateVictory list numbers.[n]
-            | _ -> callNumber (n + 1) newBoard
+            match checkedBoards with
+            | 0 -> callNumber (n + 1) newBoard
+            | _ -> checkedBoards * (numbers.[n] |> int)
         | _ -> failwith "This should not occur"
 
     callNumber 0 boards

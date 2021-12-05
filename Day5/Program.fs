320424d3eca288ec079791160c6c509c069318fb
Peter Keenan <keenan@posteo.net>
2021-12-05 22:18:18 +0000

    Minor refactoring and add notes

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -2,10 +2,8 @@ module DayFive
 
 open System.IO
 
-let buffer = seq { yield! File.ReadLines @"./Day5/exampleInput.txt" }
-
-let unwantedChars char =
-    char <> ' ' || char <> '-' || char <> '>'
+let buffer =
+    seq { yield! File.ReadLines @"./Day5/exampleInput.txt" }
 
 let turnLineIntoStarAndEndCoordinates (str: string) =
     str.Split ' '
@@ -13,16 +11,26 @@ let turnLineIntoStarAndEndCoordinates (str: string) =
     |> Array.toSeq
 
 let turnIntoTuples (input: seq<string>) =
-    let intermediary = 
+    let intermediary =
         input
         |> Seq.map ((fun (x: string) -> x.Split(',')) >> seq)
-        |> Seq.map (Seq.map int)
-        |> Seq.concat
+        |> Seq.collect (Seq.map int)
         |> Seq.toList
-    seq [(intermediary.[0], intermediary.[1]); (intermediary.[2], intermediary.[3])]
+
+    seq [ (intermediary.[0], intermediary.[1])
+          (intermediary.[2], intermediary.[3]) ]
+    
+    
+// produce seq per start/end pair
+// flatten sequences
+// map coordinate to +1 on grid
+// (grid inits all 0s)
 
 let parseBuffer =
     buffer
-    |> Seq.map (turnLineIntoStarAndEndCoordinates >> turnIntoTuples)
+    |> Seq.map (
+        turnLineIntoStarAndEndCoordinates
+        >> turnIntoTuples
+    )
 
-parseBuffer
+// parseBuffer

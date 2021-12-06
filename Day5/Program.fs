094f96f3cd9ea3112c16540026933fc86fda946c
Peter Keenan <peter.keenan@sage.com>
2021-12-06 15:41:24 +0000

    Finish tuple range logic

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -6,12 +6,7 @@ open Microsoft.FSharp.Core
 let buffer =
     seq { yield! File.ReadLines @"./Day5/exampleInput.txt" }
 
-type Coordinate =
-    Coordinate of x: int * y: int
-    
-type CoordinateSet =
-    | Start of Coordinate
-    | End of Coordinate
+type Coordinate = Coordinate of x: int * y: int
 
 let turnLineIntoStarAndEndCoordinates (str: string) =
     str.Split ' '
@@ -25,55 +20,53 @@ let turnIntoTuples (input: seq<string>) =
         |> Seq.collect (Seq.map int)
         |> Seq.toList
 
-    seq [ Start (Coordinate (intermediary.[0], intermediary.[1]))
-          End (Coordinate (intermediary.[2], intermediary.[3])) ]
-    
-    
+    seq [ (intermediary.[0], intermediary.[1])
+          (intermediary.[2], intermediary.[3]) ]
+
+
 // produce seq per start/end pair
 // flatten sequences
 // map coordinate to +1 on grid
 // (grid inits all 0s)
 
 let completeRanges (xs: int list, ys: int list) =
-//    let fullLength = max 
-    let createList (list: int list) :int list =
-        match list.[0], list.[1] with
-        | x1, x2 when x1 = x2 -> [x1]
-        | x1, x2 when x1 > x2 -> [ x2 .. x1 ]
-        | x1, x2 when x1 < x2 -> [x1..x2]
-        | _ -> failwith "todo"
-        
-    let xs' = createList xs
-    let ys' = createList ys
-    
-    
-//    match xs'.Length, ys'.Length with
-//    | xs'', _ when xs'' < fullLength -> List.init fullLength (fun _ -> xs')
-//    | _, ys'' when ys'' < fullLength -> List.init fullLength (fun _ -> ys')
-//    | xs'', _ when xs'' = fullLength -> xs'
-//    | _, ys'' when ys'' = fullLength -> ys'
-    
-    ys' |> List.zip xs'
-    
-//    seq {
-//            
-//        
-//    }
+    let fullLength =
+        match xs, ys with
+        | [ x; x' ], [ y; y' ] when x = x' -> max y y' - min y y'
+        | [ x; x' ], [ y; y' ] when y = y' -> max x x' - min x x'
+        | [ x; x' ], [ y; _ ] 
+            when x > y && x > x' -> x - max x' y
+        | _ -> failwith "fullLength todo"
 
-let parseBuffer =
+    let createList (list: int list) : int list =
+        match list.[0], list.[1] with
+        | x1, x2 when x1 = x2 -> List.init (fullLength + 1) (fun _ -> x1)
+        | x1, x2 when x1 > x2 ->
+            [ x1 .. -1 .. x2 ]
+        | x1, x2 when x1 < x2 -> [ x1 .. x2 ]
+        | _ -> failwith "createList todo"
+
+    createList ys
+    |> List.zip (createList xs)
+
+let parseBuffer () =
     let tuples =
         buffer
         |> Seq.map (
             turnLineIntoStarAndEndCoordinates
             >> turnIntoTuples
         )
-        
+
     seq {
-           for coordinateSet in tuples do
-               coordinateSet |> Seq.toList |> List.unzip
-               }
+        for coordinateSet in tuples do
+            coordinateSet |> Seq.toList |> List.unzip
+    }
+//    |> Seq.toList
     |> Seq.map completeRanges
-    
+//    |> List.concat
     
 
-// parseBuffer
+
+let linesToProcess = parseBuffer ()
+
+// linesToProcess

8ec916fc9f4f6a0538bcdefdb2900c22fca76c92
Peter Keenan <keenan@posteo.net>
2021-12-08 08:10:33 +0000

    Poor logic for growth

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -16,27 +16,25 @@ let buffer file =
     |> List.filter Char.IsNumber
     |> List.map (string >> int >> ageTracker)
 
-// buffer @"./Day6/exampleInput.txt"
-
-
 let state (internalTimer,age) =
     match (internalTimer, age) with
     | 0, Old -> 6, Old
     | 0, New -> 8, Old
     | _, age -> internalTimer - 1, age
     
-let growth x =
-    match x with
-    | 0 -> Some (List.init 0 (fun _ -> 0))
-    | _ -> None
+let growth currentState newState =
+    match currentState, newState with
+    | currentState, _ when currentState |> List.contains (0, Old) -> newState |> List.append [(8, New)]
+    | _ -> newState
 
 let lanternFishExponentialGrowth initialState =
     let rec stateManager currentState counter =
         printfn $"counter: {counter}"
         let newState = currentState |> List.map state
+        let newNewState = growth currentState newState
         match counter with
         | n when n > 0 ->
-            stateManager newState (counter - 1)
+            stateManager newNewState (counter - 1)
         | _ -> currentState |> List.map state |> List.map fst
         
     stateManager initialState 80

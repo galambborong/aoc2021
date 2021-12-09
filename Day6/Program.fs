89d1f91238e49129754988f267d3624de39f5ad4
Peter Keenan <peter.keenan@sage.com>
2021-12-09 20:34:43 +0000

    Considering mutable variable

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -12,32 +12,39 @@ let ageTracker i = (i, Old)
 let buffer file =
     seq { yield! File.ReadLines file }
     |> Seq.head
-    |> Seq.toList
-    |> List.filter Char.IsNumber
-    |> List.map (string >> int >> ageTracker)
+    |> Seq.toArray
+    |> Array.filter Char.IsNumber
+    |> Array.map (string >> int >> ageTracker)
 
 let state (internalTimer: int,age: Age) =
     match internalTimer with
     | 0 -> 6, Old
     | _ -> (internalTimer - 1),age
     
-let growth currentState newState =
-    let zeros = currentState |> List.filter (fun (x: int,_) -> x = 0)
-    
-    newState @ List.init zeros.Length (fun _ -> (8,New))
+let growth currentState (newState: (int * Age)[]) =
+    let zeros = currentState |> Array.filter (fun (x: int,_) -> x = 0)
+    newState |> Array.append (Array.init zeros.Length (fun _ -> (8,New)))
 
-let lanternFishExponentialGrowth initialState =
-    let rec stateManager currentState counter =
+let lanternFishExponentialGrowth  =
+    let mutable stateTrack = buffer @"./Day6/exampleInput.txt"
+    let rec stateManager counter =
         printfn $"{counter}"
-        let newState = currentState |> List.map state
-        let newNewState = growth currentState newState
+        stateTrack <- growth stateTrack (stateTrack |> Array.map state)
         match counter with
         | n when n > 0 ->
-            stateManager newNewState (counter - 1)
-        | _ -> newNewState |> List.map fst |> List.length
+            stateManager (counter - 1)
+        | _ -> stateTrack |> Array.length
         
-    stateManager initialState 255
-        
+    
+    stateManager 255
+    
+    
+//    let stateManager' currentState nn =
+//        printfn $"{nn}"
+//        growth currentState (currentState |> Array.map state)
+//        
+//    let endResult = [0..255] |> List.fold stateManager' initialState
+//    endResult.Length
         
 // lanternFishExponentialGrowth (buffer @"./Day6/actualInput.txt")
 // lanternFishExponentialGrowth (buffer @"./Day6/exampleInput.txt")

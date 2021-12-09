94d0d29260d5e8a799b8de648c64d7863f197ccf
Peter Keenan <keenan@posteo.net>
2021-12-09 20:54:42 +0000

    General capture

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -21,22 +21,25 @@ let state (internalTimer: int,age: Age) =
     | 0 -> 6, Old
     | _ -> (internalTimer - 1),age
     
-let growth currentState (newState: (int * Age)[]) =
-    let zeros = currentState |> Array.filter (fun (x: int,_) -> x = 0)
-    newState |> Array.append (Array.init zeros.Length (fun _ -> (8,New)))
 
-let lanternFishExponentialGrowth  =
+let lanternFishExponentialGrowth n =
     let mutable stateTrack = buffer @"./Day6/exampleInput.txt"
-    let rec stateManager counter =
+    
+    let growth currentState (newState: (int * Age)[]) =
+        let zeros = currentState |> Array.filter (fun (x: int,_) -> x = 0)
+        newState |> Array.append (Array.init zeros.Length (fun _ -> (8,New)))
+    
+    let mutable counter = n
+    while counter > 0 do
         printfn $"{counter}"
         stateTrack <- growth stateTrack (stateTrack |> Array.map state)
-        match counter with
-        | n when n > 0 ->
-            stateManager (counter - 1)
-        | _ -> stateTrack |> Array.length
+        counter <- counter - 1
+//        match counter with
+//        | n when n > 0 ->
+//            stateManager (counter - 1)
+//        | _ -> stateTrack |> Array.length
         
     
-    stateManager 255
     
     
 //    let stateManager' currentState nn =
@@ -48,3 +51,4 @@ let lanternFishExponentialGrowth  =
         
 // lanternFishExponentialGrowth (buffer @"./Day6/actualInput.txt")
 // lanternFishExponentialGrowth (buffer @"./Day6/exampleInput.txt")
+// lanternFishExponentialGrowth 255

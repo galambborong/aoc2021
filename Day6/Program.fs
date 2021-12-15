57caece9f615843db87e1b9e9399e6818e435903
Peter Keenan <keenan@posteo.net>
2021-12-15 21:11:19 +0000

    Looking at dictionary

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -1,16 +1,19 @@
 module DaySix
 
 open System
+open System.Collections.Generic
 open System.IO
 
+
+type Fishes = Map<int, int>
 
 
 let buffer file =
     seq { yield! File.ReadLines file }
     |> Seq.head
-    |> Seq.toArray
-    |> Array.filter Char.IsNumber
-    |> Array.map (string >> int)
+    |> Seq.toList
+    |> List.filter Char.IsNumber
+    |> List.map (string >> int)
 
 let state (internalTimer: int) =
     match internalTimer with
@@ -18,34 +21,21 @@ let state (internalTimer: int) =
     | _ -> internalTimer - 1
     
 
-let lanternFishExponentialGrowth n =
-    let mutable stateTrack = buffer @"./Day6/exampleInput.txt"
+let lanternFishExponentialGrowth () =
+    let stateTrack = buffer @"./Day6/exampleInput.txt"
+   
+    let mutable dict = Dictionary<int, int>()
+            
+    for i in 0..stateTrack.Length - 1 do
+        dict.Add(i, stateTrack.[i]) |> ignore
+            
+    printfn $"{dict.Values}"
+    for i in 0..79 do
+        for ob in dict do
+            dict.[ob.Key] <- state ob.Value
+            if dict.[ob.Key] = 0 then do
+                dict.Add(dict.Count + 1, 8)
     
-    let growth currentState (newState: int []) =
-        let zeros = currentState |> Array.filter (fun (x: int) -> x = 0)
-        newState |> Array.append (Array.init zeros.Length (fun _ -> 8))
+    dict.Count
     
-    let mutable counter = n
-    while counter > 0 do
-        printfn $"counter: {counter} -- length: {stateTrack.Length}"
-        stateTrack <- growth stateTrack (stateTrack |> Array.map state)
-        counter <- counter - 1
-//        match counter with
-//        | n when n > 0 ->
-//            stateManager (counter - 1)
-//        | _ -> stateTrack |> Array.length
-    stateTrack
-        
-    
-    
-    
-//    let stateManager' currentState nn =
-//        printfn $"{nn}"
-//        growth currentState (currentState |> Array.map state)
-//        
-//    let endResult = [0..255] |> List.fold stateManager' initialState
-//    endResult.Length
-        
-// lanternFishExponentialGrowth (buffer @"./Day6/actualInput.txt")
-// lanternFishExponentialGrowth (buffer @"./Day6/exampleInput.txt")
-let x = lanternFishExponentialGrowth 256 |> Array.length
+// lanternFishExponentialGrowth()

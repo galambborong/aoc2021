afe4bba568597bbb0da2716f2b9c389a9e00735e
Peter Keenan <keenan@posteo.net>
2021-12-16 08:40:32 +0000

    Dictionary logic not quite there

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -20,11 +20,15 @@ let state (internalTimer: int) =
     | 0 -> 6
     | _ -> internalTimer - 1
     
+    
+let countZeros (dict: Dictionary<int, int>) =
+    dict.Values
 
 let lanternFishExponentialGrowth () =
-    let stateTrack = buffer @"./Day6/exampleInput.txt"
+    let stateTrack = buffer @"/home/pk/Repos/AdventOfCode2021/Day6/exampleInput.txt"
    
     let mutable dict = Dictionary<int, int>()
+    let mutable inter = Dictionary<int,int>()
             
     for i in 0..stateTrack.Length - 1 do
         dict.Add(i, stateTrack.[i])
@@ -33,9 +37,13 @@ let lanternFishExponentialGrowth () =
         for ob in dict do
             dict.[ob.Key] <- state ob.Value
             if dict.[ob.Key] = 0 then do
-                printfn $"Hello from inside"
-//                dict.Add(dict.Count + 1, 8)
+                inter.[dict.Count + inter.Count] <- 8
+            
+        for ob in inter do
+            dict[ob.Key] <- ob.Value
+            
+        inter.Clear()
     
     dict.Count
-    
-// lanternFishExponentialGrowth()
+
+//lanternFishExponentialGrowth()

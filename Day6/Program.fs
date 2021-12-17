11c980e07e1f74ea4c7df1d9d3524bce19812f9f
Peter Keenan <peter.keenan@sage.com>
2021-12-17 12:16:09 +0000

    Latest progress

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -27,26 +27,27 @@ let lanternFishExponentialGrowth () =
     let lanternFishes = Dictionary<int, int>()
 
     let addFishInternalTimerKey () =
-        [ 0 .. 8 ]
+        [ 0 .. 9 ]
         |> List.iter (fun x -> lanternFishes.Add(x, 0))
 
     let initialiseFishCountsFromInput () =
         stateTrack
         |> List.iter (fun i -> lanternFishes.[i] <- lanternFishes.[i] + 1)
-        
+
     let initialiseMockFishCounts () =
         lanternFishes.[3] <- 1
         lanternFishes.[5] <- 2
 
     addFishInternalTimerKey ()
     initialiseFishCountsFromInput ()
-//    initialiseMockFishCounts ()
+    //    initialiseMockFishCounts ()
 
     let countFishesOverNDays n =
         [ 0 .. n ]
         |> List.iter
             (fun _ ->
                 printfn $"\n"
+
                 for fishAge in lanternFishes do
                     printfn $"current Key {fishAge.Key}, current value {fishAge.Value}"
 
@@ -55,22 +56,38 @@ let lanternFishExponentialGrowth () =
                         match lanternFishes.[0] with
                         | 0 -> lanternFishes.[0] <- 0
                         | x when x > 0 ->
-                                          lanternFishes.[8] <- lanternFishes.[0] 
-                                          lanternFishes.[6] <- lanternFishes.[0]
-                                          lanternFishes.[0] <- 0
+                            lanternFishes.[9] <- lanternFishes.[0]
+                            lanternFishes.[0] <- 0
                         | _ -> failwith "lanternFishes.[0] edge"
+                    | 7 ->
+                        match lanternFishes.[7] with
+                        | 0 -> lanternFishes.[7] <- 0
+                        | x when x > 0 ->
+                            printfn $"6 should be {lanternFishes.[7] + lanternFishes.[9]}"
+                            lanternFishes.[6] <- lanternFishes.[7] + lanternFishes.[9]
+                            lanternFishes.[7] <- lanternFishes.[8]
+                        | _ -> failwith "lanternFishes.[7] edge"
+                    | 9 ->
+                        match lanternFishes.[9] with
+                        | 0 -> lanternFishes.[9] <- 0
+                        | x when x > 0 ->
+                            lanternFishes.[8] <- lanternFishes.[9]
+//                            lanternFishes.[6] <- lanternFishes.[9]
+                            lanternFishes.[9] <- 0
+                        | _ -> failwith "lanternFishes.[9] edge"
                     | _ ->
                         match lanternFishes.[fishAge.Key] with
                         | 0 -> lanternFishes.[fishAge.Key] <- 0
                         | x when x > 0 ->
-                                          lanternFishes.[fishAge.Key - 1] <- lanternFishes.[fishAge.Key]
-                                          lanternFishes.[fishAge.Key] <- 0
+                            lanternFishes.[fishAge.Key - 1] <- lanternFishes.[fishAge.Key]
+                            lanternFishes.[fishAge.Key] <- 0
                         | _ -> failwith "lanternFishes.[fishAge.Key] edge")
 
-    countFishesOverNDays 2
+    countFishesOverNDays 5
+
+    lanternFishes.Remove(9) |> ignore
 
     lanternFishes.Values |> Seq.sum
 
 
-
-// lanternFishExponentialGrowth()
+lanternFishExponentialGrowth ()

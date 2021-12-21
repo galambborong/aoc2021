66ca09f6a53f3f95f11058cbb12bb5a17267f05b
Peter Keenan <keenan@posteo.net>
2021-12-21 08:43:04 +0000

    Day 7 star one solution

---
+++ Day7/Program.fs
@@ -0,0 +1,26 @@
+module DaySeven
+
+open System.IO
+
+let buffer filename =
+    seq { yield! File.ReadLines filename }
+    |> Seq.head
+    |> (fun (line: string) -> Seq.toList (line.Split ','))
+    |> List.map int
+
+let horizontalPositions = buffer @"./Day7/actualInput.txt"
+
+let minPosition = horizontalPositions |> List.min
+
+let maxPosition = horizontalPositions |> List.max
+
+let differences () =
+    [ 0 .. maxPosition ]
+    |> List.map
+        (fun n ->
+            horizontalPositions
+            |> List.map (fun x -> max x n - min x n)
+            |> List.sum)
+    |> List.min
+    
+// differences ()
